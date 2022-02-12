73c8820f324cd789fe19715f2d9a26d327da8aa8
KronosGR <geo.elgeo@gmail.com>
2022-02-12 19:46:08 +0100

    added on and trigger methods for events on user

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,8 @@ import { User } from './models/User';
 const user = new User({ name: 'kronos', age: 20 });
 user.set({ age: 111 });
 
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('change #1')
+});
+
+user.trigger('change')
--- src/models/User.ts
+++ src/models/User.ts
@@ -4,9 +4,11 @@ interface UserProps {
 }
 
 // type alias
-type Callback = () => {};
+type Callback = () => void;
 
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -18,6 +20,18 @@ export class User {
   }
 
   on(eventName: string, callback: Callback) {
-    
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach((callback) => {
+      callback();
+    });
   }
 }

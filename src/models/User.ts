2b657fe43bb585a77985a5bb467a5ee394276f11
KronosGR <geo.elgeo@gmail.com>
2022-02-13 14:11:56 +0100

    added Sync model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,4 @@
-import axios, { Axios, AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
 interface UserProps {
   id?: number;
@@ -7,6 +7,8 @@ interface UserProps {
 }
 
 export class User {
+  public events:Eventing = new Eventing();
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -17,21 +19,5 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      // update user
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
+  
 }

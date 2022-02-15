330f6c5d0e3f04a4fb29a28806c1bb47df5a4936
KronosGR <geo.elgeo@gmail.com>
2022-02-15 12:37:49 +0100

    added UserForm->render()

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,12 +1,19 @@
-export class UserForm{
-  parent: Element;
+export class UserForm {
+  constructor(public parent: Element) {}
 
-  template():string{
+  template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input />
       </div>
-    `
+    `;
+  }
+
+  render(): void {
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+
+    this.parent.append(templateElement.content);
   }
 }
